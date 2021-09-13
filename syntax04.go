03590216008d449cd983a3b88f2422420771d0f2
iL4897 <ancestor_one@naver.com>
2021-09-13 19:06:26 +0900

    v0.3

--- syntax04.go
+++ syntax04.go
@@ -6,25 +6,24 @@ import (
 	"time" // seed	생성 패키지
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.2
+// 난수 추출된 수의 소수 판정 프로그램 v0.3
 // 소수 : 1과 자기 자신 이외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
-
 func main() {
 	// seed 생성
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
-	count := 0
+	isPrime := true
 	number := rand.Intn(150) + 2 // 0과 1 제외, 2~151 사이의 수
 	fmt.Println("임의로 추출된 수 : ", number)
 
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
-			count = count + 1
+			isPrime = false
 		}
 	}
 
-	if count == 2 {
+	if isPrime == true {
 		fmt.Println(number, "은(는) 소수입니다.")
 	} else {
 		fmt.Println(number, "은(는) 소수가 아닙니다.")
